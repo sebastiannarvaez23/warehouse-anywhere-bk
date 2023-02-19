913f06b3fa587a0231299fac6f3c3eb332275505
Sebastian Narvaez Lopera <narvaezsebas8@gmail.com>
2023-02-19 11:49:46 -0500

    Se realiza ajuste en el modelo box para que cuando se modifique un registro, la fecha ultima modificación debe actualizarse tambien.

--- box/models.py
+++ box/models.py
@@ -1,7 +1,7 @@
 # Django
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils.timezone import now
+from django.utils import timezone
 
 # Local apps
 from picking.models import Picking
@@ -22,7 +22,7 @@ class Dimension(models.Model):
 
 class Box(models.Model):
     id = models.AutoField(primary_key=True, verbose_name="Id")
-    last_modification = models.DateTimeField(verbose_name="Última modificación", default=now)
+    last_modification = models.DateTimeField(verbose_name="Última modificación", default=timezone.now)
     gross_weight = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Peso Bruto")
     responsible = models.ForeignKey(User, on_delete=models.DO_NOTHING, verbose_name="Responsable")
     dimension = models.ForeignKey(Dimension, on_delete=models.DO_NOTHING, verbose_name="Dimensión")
@@ -36,3 +36,7 @@ class Box(models.Model):
     def __str__(self):
         return self.dimension.name
 
+    def save(self, *args, **kwargs):
+        # Actualiza la fecha y hora de la última modificación
+        self.last_modification = timezone.now()
+        super(Box, self).save(*args, **kwargs)
