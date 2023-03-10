4c51ca5a47c5a452551a3e0aa954e74ea2a3bd52
Sebastian Narvaez Lopera <narvaezsebas8@gmail.com>
2023-03-10 00:16:48 -0500

    Se termina el modelo User para lograr la autenticacion

--- registration/api/serializers.py
+++ registration/api/serializers.py
@@ -67,14 +67,25 @@ class UserSignUpSerializer(serializers.Serializer):
         user = User.objects.create_user(**data)
         return user
 
-class UserLoginSerializer(serializers.ModelSerializer):
+class UserLoginSerializer(serializers.Serializer):
     """User login Serializer.
-
     Handle the login request data.
     """
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'email', 'first_name', 'last_name')
+    username = serializers.CharField()
+    password = serializers.CharField(min_length=8, max_length=64)
+
+    def validate(self, data):
+        """Check credentials."""
+        user = authenticate(username=data['username'], password=data['password'])
+        if not user:
+            raise serializers.ValidationError('Invalid credentials')
+        self.context['user'] = user
+        return data
+
+    def create(self, data):
+        """Generate or retrive new token."""
+        token, created = Token.objects.get_or_create(user=self.context['user'])
+        return self.context['user'], token.key
 
 
 class AccountVerificationSerializer(serializers.Serializer):
--- registration/models.py
+++ registration/models.py
@@ -1,44 +1,36 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 # Create your models here.
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, username, first_name, last_name, telephone, rol, picture=None, password=None):
-        if not email:
-            raise ValueError('El usuario debe tener un correo electronico')
+    def _create_user(self, username, email, first_name, last_name, telephone, password, is_staff, is_superuser, **extra_fields):
         user = self.model(
             username=username,
-            email=self.normalize_email(email),
+            email=email,
             first_name=first_name,
             last_name=last_name,
             telephone=telephone,
-            picture=picture,
-            rol=rol
+            rol=Rol.objects.get(id=1),
+            is_staff=is_staff,
+            is_superuser=is_superuser,
+            **extra_fields
         )
         user.set_password(password)
-        user.save()
+        user.save(using=self.db)
         return user
 
-    def create_superuser(self, email, username, first_name, last_name, telephone, password):
-        user = self.create_user(
-            email=email,
-            username=username,
-            first_name=first_name,
-            last_name=last_name,
-            telephone=telephone,
-            rol=Rol.objects.get(id=1)
-        )
-        user.is_staff = True
-        user.is_superuser = True
-        user.save()
-        return user
+    def create_user(self, username, email, first_name, last_name, telephone, password=None, **extra_fields):
+        return self._create_user(username, email, first_name, last_name, telephone, password, False, False, **extra_fields)
+    
+    def create_superuser(self, username, email, first_name, last_name, telephone, password=None, **extra_fields):
+        return self._create_user(username, email, first_name, last_name, telephone, password, True, True, **extra_fields)
 
 class Rol(models.Model):
     id = models.AutoField(primary_key=True, verbose_name="id")
     name = models.CharField(max_length=200, verbose_name="Nombre Rol")
 
-class User(AbstractUser):
+class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=200, unique=True, verbose_name="Nombre de Usuario")
     email = models.EmailField(max_length=254, unique=True, verbose_name="Correo electronico")
     first_name = models.CharField(max_length=100, blank=True, null=True)
@@ -46,6 +38,8 @@ class User(AbstractUser):
     rol = models.ForeignKey(Rol, on_delete=models.PROTECT, null=True, blank=True, verbose_name="Rol")
     telephone = models.CharField(max_length=100, verbose_name="Telefono")
     picture = models.ImageField(upload_to='perfil/', max_length=200, blank=True, null=True, verbose_name="Foto de perfil")
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
     objects = UserManager()
 
     USERNAME_FIELD = 'username'
@@ -58,10 +52,3 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
-    
-    def save(self, *args, **kwargs):
-        if self.is_superuser:
-            self.rol = Rol.objects.get(id=1)
-        elif not self.rol:
-            self.rol = Rol.objects.get(id=2)
-        super().save(*args, **kwargs)
