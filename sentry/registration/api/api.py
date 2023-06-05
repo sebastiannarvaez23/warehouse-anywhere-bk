9292237ac5e0b19eff9d66e3bd02f4930e331bc6
Sebastian Narvaez Lopera <narvaezsebas8@gmail.com>
2023-06-05 12:12:38 -0500

    Se ajusta api de autenticacion y generacion de token para que el endpoint devuelva el dominio al que tiene que consultar todo el usuario

--- sentry/registration/api/api.py
+++ sentry/registration/api/api.py
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+from sentry.company.models import Company, Domain
 from sentry.registration.api.serializers import (
     UserLoginSerializer,
     UserModelSerializer,
@@ -14,6 +15,8 @@ from sentry.registration.api.serializers import (
 )
 
 # Create your views here.
+
+
 class UserLoginAPIView(APIView):
     """ Users login API View """
 
@@ -22,18 +25,22 @@ class UserLoginAPIView(APIView):
         serializer = UserLoginSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user, token = serializer.save()
+        user_company = Company.objects.get(user=user)
+        user_domain_company = Domain.objects.get(id=user_company.id)
         data = {
             'status': 200,
             'user': {
                 'id': user.id,
-                'username': user.username
+                'username': user.username,
+                'domain': user_domain_company.domain
             },
             'access_token': token
         }
         response = Response(data, status=status.HTTP_200_OK)
         response.set_cookie('access_token', token, httponly=True)
         return response
-    
+
+
 class UserSignUpAPIView(APIView):
     """ Users sign up API View """
 
